de49e87c2a6160714c21e78827765c10cb7ccd2c
Tony Galmiche <tony.galmiche@infosaone.com>
2022-06-30 16:42:36 +0200

    modifs

--- models/purchase.py
+++ models/purchase.py
@@ -5,3 +5,18 @@ class purchase_order(models.Model):
     _inherit = "purchase.order"
 
     is_condition_tarifaire = fields.Text('Conditions tarifaire', related='partner_id.is_condition_tarifaire')
+
+
+class purchase_order_line(models.Model):
+    _inherit = "purchase.order.line"
+
+    is_largeur = fields.Float('Largeur')
+    is_hauteur = fields.Float('Hauteur')
+
+
+    @api.onchange('is_largeur','is_hauteur')
+    def onchange_calculateur(self):
+        for obj in self:
+            print(obj)
+            if  obj.is_largeur and obj.is_hauteur:
+                obj.product_qty = obj.is_largeur*obj.is_hauteur
